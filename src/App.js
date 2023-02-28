e5fb6c26315d8c2413aed4ed28b69160bb55ef78
Carlos Rivas <Cjrivas00@gmail.com>
2023-02-28 14:16:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>dasdokaso</h1>
+    </>
+  );
+}
+
+export default App;
