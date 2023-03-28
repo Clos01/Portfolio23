7fa395ec95b9e6176a75d9a28cd043e55022a774
Carlos Rivas <Cjrivas00@gmail.com>
2023-03-27 23:13:26 -0400

    fixed some issues for mobile

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -2,57 +2,50 @@ import React from "react";
 
 export const Aboutme = () => {
   return (
-    
-      <div className="bg-[#3f6e94c3] lg:h-[56rem] h-auto mx-auto  px-2 w-full overflow-y-hidden ">
-        <div className="flex flex-col lg:flex-row items-stretch justify-between lg:px-0 px-6 lg:py-20 py-8 2xl:mx-auto 2xl:container">
-      
-          <div className="bg-[#d3a56598] lg:w-1/2 lg:ml-12 lg:p-14 p-10 flex items-center">
-            <div>
-              <h1 className="dark:text-white md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-gray-800 capitalize text-center">
-                About me{" "}
-              </h1>
-              <div className="dark:text-gray-300 md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12  leading-normal text-gray-600 mt-5 text-xl">
-                <p>
-                  {" "}
-                  Greetings! I am a highly motivated software developer who
-                  possesses a passion for collaborative work that results in
-                  innovative solutions. My eagerness to learn and tackle new
-                  challenges is matched only by my appreciation for constructive
-                  feedback that helps me grow and develop my skills.
-                </p>{" "}
-                <br />
-                <p>
-                  I am well-versed in computer science as applied to JavaScript,
-                  including knowledge of algorithms such as searches, sorts,
-                  efficiency Additionally,
-                  I have gained experience working with NoSql databases,
-                  Progressive Web Applications (PWAs), MERN stack development,
-                  state management, and Express.js. I am committed to expanding
-                  my knowledge in object-oriented programming and SQL and other
-                  areas beyond my current skillset. Thank you for your
-                  consideration.
-                </p>
-                <br />
-                <div class="flex flex-row">
-                  <div class="flex w-10 h-10">
-                    <img src="https://cdn-icons-png.flaticon.com/512/1051/1051277.png" alt="" />
-                    <img src="https://cdn-icons-png.flaticon.com/512/919/919851.png" alt="" />
-                    <img src="https://cdn-icons-png.flaticon.com/512/2772/2772128.png" alt="" />   
-                    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" alt="" />
-                    <img src="https://cdn-icons-png.flaticon.com/512/919/919825.png" alt="" />
-                    <img src="https://t4.ftcdn.net/jpg/00/90/67/37/240_F_90673721_nTq4hQ0UG1RxQ1niYwMnhzp05fsdkZyN.jpg" alt="" />
-                  </div>
-                </div>
+    <div className="bg-[#3f6e94c3] lg:h-[56rem] h-auto mx-auto px-2 w-full overflow-y-hidden">
+      <div className="flex flex-col lg:flex-row items-stretch justify-between lg:px-0 px-6 lg:py-20 py-8 2xl:mx-auto 2xl:container">
+        <div className="bg-[#d3a56598] lg:w-1/2 lg:ml-12 lg:p-14 p-10 flex items-center">
+          <div>
+            <h1 className="dark:text-white md:w-8/12 lg:w-10/12 xl:8/12 2xl:w-8/12 w-full xl:text-6xl sm:text-5xl text-4xl font-semibold text-gray-800 capitalize text-center">
+              About me
+            </h1>
+            <div className="dark:text-gray-300 md:w-9/12 lg:w-11/12 xl:w-10/12 2xl:9/12 leading-normal text-gray-600 mt-5 text-xl">
+              <p>
+                Greetings! I am a highly motivated software developer who
+                possesses a passion for collaborative work that results in
+                innovative solutions. My eagerness to learn and tackle new
+                challenges is matched only by my appreciation for constructive
+                feedback that helps me grow and develop my skills.
+              </p>
+              <br />
+              <p>
+                I am well-versed in computer science as applied to JavaScript,
+                including knowledge of algorithms such as searches, sorts,
+                efficiency Additionally,
+                I have gained experience working with NoSql databases,
+                Progressive Web Applications (PWAs), MERN stack development,
+                state management, and Express.js. I am committed to expanding
+                my knowledge in object-oriented programming and SQL and other
+                areas beyond my current skillset. Thank you for your
+                consideration.
+              </p>
+              <br />
+              <div className="flex flex-row flex-wrap gap-2">
+                <img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/1051/1051277.png" alt="" />
+                <img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/919/919851.png" alt="" />
+                <img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/2772/2772128.png" alt="" />
+                <img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" alt="" />
+                <img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/919/919825.png" alt="" />
+                <img className="w-10 h-10" src="https://t4.ftcdn.net/jpg/00/90/67/37/240_F_90673721_nTq4hQ0UG1RxQ1niYwMnhzp05fsdkZyN.jpg" alt="" />
+              </div>
 
- <button className="dark:bg-white dark:hover:bg-gray-400 dark:hover:text-gray-100 dark:text-gray-800 sm:w-auto w-full mt-8 text-base justify-between focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none hover:bg-gray-700 font-medium leading-none text-white py-4 px-8 bg-gray flex items-center">
-  Explore
+              <button className="dark:bg-white dark:hover:bg-gray-400 dark:hover:text-gray-100 dark:text-gray-800 sm:w-auto w-full mt-8 text-base justify-between focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none hover:bg-gray-700 font-medium leading-none text-white py-4 px-8 bg-gray flex items-center">
+Explore
 </button>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-
+</div>
+</div>
+</div>
+</div>
+</div>
+);
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,19 @@ import port from "../images/port.jpg";
 
 export const Hero = () => {
   return (
-    <div className="shadow-lg h-auto mx-auto w-full overflow-hidden bg-[#85673c9a] ">
-      <div className="sm:grid sm:grid-cols-2">
+    <div className="shadow-lg h-auto mx-auto w-full overflow-hidden bg-gradient-to-r from-[#85673c9a] to-[#6a4f3080] pt-16 sm:pt-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4 sm:p-0">
         <div className="py-6 px-4 sm:p-10 text-center sm:text-left">
-          <h1 className="text-5xl font-light sm:text-5xl sm:mb-4">Hi, I'm Carlos</h1>
-          <p className="text-gray-100 font-light text-2xl sm:text-3xl">
+          <h1 className="text-4xl font-light sm:text-5xl sm:mb-4 text-white">Hi, I'm Carlos</h1>
+          <p className="text-gray-100 font-light text-xl sm:text-2xl md:text-3xl">
             Experienced full-stack developer skilled in React, HTML, CSS, and JavaScript, seeking new opportunities to apply my skills and collaborate with other talented developers.
           </p>
         </div>
-        <div className="sm:py-10">
+        <div className="sm:py-10 flex justify-center items-center">
           <img
             alt="Student"
             src={port}
-            className="  sm:rounded-r-lg w-[25rem]"
+            className="w-full sm:w-[25rem] max-w-full sm:rounded-r-lg"
           />
         </div>
       </div>
