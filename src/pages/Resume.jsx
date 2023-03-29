84dc868a811545118c44c69d168d5b5bf15f0d03
Carlos Rivas <Cjrivas00@gmail.com>
2023-03-29 15:43:36 -0400

    redesign for resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -21,7 +21,7 @@ export const Resume = () => {
       </p>
 
       <a
-     href="/resume23.pdf"  download={true}
+     href="/resume2023.pdf"  download={true}
         className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
       >
       View Resume
