5e4adf078b8560e81ae08fe3c651c91ebb0b39d0
Carlos Rivas <Cjrivas00@gmail.com>
2023-04-01 10:38:27 -0400

    added new styles to the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import port from "../images/port.jpg";
-import { motion } from 'framer-motion'; // Imported motion from framer-motion
+import { motion } from 'framer-motion';
 
-// Define animation variants for the text and image
 const textVariant = {
-  hidden: { opacity: 0, x: -50 },
+  hidden: { opacity: 0, x: -12 },
   visible: { opacity: 1, x: 0, transition: { duration: 0.5 } }
 };
 
 const imageVariant = {
-  hidden: { opacity: 0, x: 50 },
+  hidden: { opacity: 0, x: 20 },
   visible: { opacity: 1, x: 0, transition: { duration: 0.5 } }
 };
 
@@ -18,7 +17,7 @@ export const Hero = () => {
     <div
       className="shadow-lg h-auto mx-auto w-full overflow-hidden pt-16 sm:pt-0"
       style={{
-        backgroundImage: 'linear-gradient(135deg, #2ab2ec, #038697)',
+        backgroundImage: 'linear-gradient(135deg, #2ab2ec, #20c6c6, #038697)',
         backgroundRepeat: 'no-repeat',
       }}
     >
@@ -29,7 +28,7 @@ export const Hero = () => {
               Hi! I'm Carlos Rivas Juarez,
             </h1>
             <p className="text-white text-xl sm:text-2xl font-bold md:text-3xl">
-  
+            As a bilingual full-stack developer and UNC-Chapel Hill Coding Boot Camp graduate, I'm Carlos Rivas Juarez. Specializing in the MERN stack, I'm passionate about creating dynamic web applications and enhancing user experiences. With a strong background in customer service and project collaboration, I'm poised to make a positive impact in tech.
             </p>
           </motion.div>
         </div>
@@ -40,7 +39,7 @@ export const Hero = () => {
             variants={imageVariant}
             alt="Student"
             src={port}
-            className="w-full sm:w-[25rem] max-w-full sm:rounded-r-lg"
+            className="w-full sm:w-[25rem] max-w-full sm:rounded-full" // Added "rounded-full" class here
           />
         </div>
       </div>
