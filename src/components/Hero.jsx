e9fbd14e2af39e8e7f09789261dd8204abac7a4c
Carlos Rivas <Cjrivas00@gmail.com>
2023-04-01 02:02:44 -0400

    added animation and color change to contact form also added animation to hero with color change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import port from "../images/port.jpg";
+import Fade from 'react-reveal/Fade'; // Import Fade animation
 
 export const Hero = () => {
   return (
-    <div className="shadow-lg h-auto mx-auto w-full overflow-hidden bg-gradient-to-r from-[#bed7e2e8] to-[#087786ca] pt-16 sm:pt-0">
+    <div
+      className="shadow-lg h-auto mx-auto w-full overflow-hidden pt-16 sm:pt-0"
+      style={{
+        backgroundImage: 'linear-gradient(135deg, #2ab2ec, #038697)',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4 sm:p-0">
         <div className="py-6 px-4 sm:p-10 text-center sm:text-left">
-          <h1 className="text-4xl font-light sm:text-5xl sm:mb-4 text-gray-900 ">Hi! I'm Carlos Rivas Juarez,</h1>
-          <p className="text-gray-700  text-xl sm:text-2xl font-bold md:text-3xl">
-          an experienced full-stack web developer and graduate of UNC-Chapel Hill's The Coding Boot Camp. My technical expertise includes proficiency in React, HTML, CSS, JavaScript, Git, and other web development technologies. I'm passionate about crafting unique and visually stunning user experiences, and I'm actively seeking new opportunities to apply my skills, collaborate with fellow developers, and contribute to meaningful projects in the tech industry.
-          </p>
+          <Fade left>
+            <h1 className="text-4xl font-light sm:text-5xl sm:mb-4 text-white">
+              Hi! I'm Carlos Rivas Juarez,
+            </h1>
+            <p className="text-white text-xl sm:text-2xl font-bold md:text-3xl">
+              An experienced full-stack web developer and graduate of UNC-Chapel Hill's The Coding Boot Camp. My technical expertise includes proficiency in React, HTML, CSS, JavaScript, Git, and other web development technologies. I'm passionate about crafting unique and visually stunning user experiences, and I'm actively seeking new opportunities to apply my skills, collaborate with fellow developers, and contribute to meaningful projects in the tech industry.
+            </p>
+          </Fade>
         </div>
         <div className="sm:py-10 flex justify-center items-center">
-          <img
-            alt="Student"
-            src={port}
-            className="w-full sm:w-[25rem] max-w-full sm:rounded-r-lg"
-          />
+          <Fade right>
+            <img
+              alt="Student"
+              src={port}
+              className="w-full sm:w-[25rem] max-w-full sm:rounded-r-lg"
+            />
+          </Fade>
         </div>
       </div>
     </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import RubberBand from 'react-reveal/RubberBand';
 
 const Contact = () => {
+  const [animateButton, setAnimateButton] = useState(false);
+
+  const handleAnimationEnd = () => {
+    setAnimateButton(true);
+  };
+
   return (
-    <div name='Contact' className='w-full h-screen bg-[#112646] flex justify-center items-center pt-30 sm:pt-0 p-35'>
+    <div
+      name='Contact'
+      className='w-full h-screen flex justify-center items-center pt-30 sm:pt-0 p-35'
+      style={{
+        backgroundImage: 'linear-gradient(135deg, #2ab2ec, #038697)',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <form method='POST' action="https://getform.io/f/07c4747d-9e0c-4efa-8cf7-dfc955885eaa" className='flex flex-col max-w-[600px] w-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-          <p className='text-gray-300 py-4'> Submit the form below or shoot me an email - Cjrivas00@gmail.com</p>
+        <div className='pb-900'>
+          <Zoom>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-700'>Contact</p>
+          </Zoom>
+          <Fade left delay={200}>
+            <p className='text-gray-00 py-4'> Submit the form below or shoot me an email - Cjrivas00@gmail.com</p>
+          </Fade>
         </div>
-        <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-        <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-        <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Contact Me</button>
+        <Fade right delay={400}>
+          <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+        </Fade>
+        <Fade right delay={600}>
+          <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+        </Fade>
+        <Fade right delay={800} onReveal={handleAnimationEnd}>
+          <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+        </Fade>
+        {animateButton ? (
+          <RubberBand>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Contact Me</button>
+          </RubberBand>
+        ) : (
+          <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Contact Me</button>
+        )}
       </form>
     </div>
   )
