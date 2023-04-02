f6370d8706871f8bb6280d89f9b01a1cfc00c95b
Carlos Rivas <Cjrivas00@gmail.com>
2023-04-02 14:23:06 -0400

    fixed skill import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Hero } from "../components/Hero";
-import Skills from "../components/Skills";
+import Skills from "../components/skills";
 import Education from "../components/Education";
 const Home = () => {
   return (
